578506b5a67254383c2289a16ed8d0e549aca7dc
Abdulsalamdev27 <Abubakaabdulsalam691@gmail.com>
2026-02-11 08:18:33 -0800

    signup controller added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,15 +1,13 @@
 import express from 'express'
+import { signUp } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
-router.get("/signup", (req, res)=>{
-    res.send("signup")
+router.post("/signup", signUp)
+// router.get("/login", (req, res)=>{
+//     res.send("login")
 
-})
-router.get("/login", (req, res)=>{
-    res.send("login")
-
-})
+// })
 
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,17 @@
 import express from "express";
-import path from "path";
 import dotenv from "dotenv";
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 
 dotenv.config();
 
 const app = express();
+app.use(express.json()) //middleware req.bod
+
 const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
@@ -43,4 +47,5 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
+  connectDB();
 });
