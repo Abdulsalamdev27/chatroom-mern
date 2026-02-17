fdcffef2f8f3963e84825be2071c0352d2e860cb
abdulsalamdev27 <abubakaabdulsalam691@gmail.com>
2026-02-17 06:33:27 -0800

    login and logout functionality added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -71,3 +71,38 @@ const { fullName, email, password } = req.body
         })
     }
 }
+
+export const login = async (req, res)=>{
+    const {email, password} = req.body
+
+    try{
+        const user = await User.findOne({email});
+        if (!user) return res.status(400).json({message: "Invaild credentials"});
+        //never tell the client which one is incorrect
+
+        const isPasswordCorrect = await bcrypt.compare(password,user.password)
+        if(!isPasswordCorrect) return res.status(400).json({message: "Invaild credentials"});
+
+        generateToken(user._id, res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+
+        });
+    }catch(error){
+        console.error("Error in login controller:", error);
+        res.status(500).json({message: "internal server error"});
+
+    }
+
+}
+
+export const logout = (_, res)=>{
+    res.cookie("jwt", "",{maxAge:0})
+    res.status(200).json({
+        message: "Logged out successfully"
+    })
+};
