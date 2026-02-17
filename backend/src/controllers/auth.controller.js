ebca9988fbf549f17decb2cb6636184875d30c45
abdulsalamdev27 <abubakaabdulsalam691@gmail.com>
2026-02-17 06:41:45 -0800

    login refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -75,6 +75,12 @@ const { fullName, email, password } = req.body
 export const login = async (req, res)=>{
     const {email, password} = req.body
 
+    if(!email || !password){
+        res.status(400).json({
+            message: "Email and password are required"
+        });
+    }
+
     try{
         const user = await User.findOne({email});
         if (!user) return res.status(400).json({message: "Invaild credentials"});
